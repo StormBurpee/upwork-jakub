e46d43d5f7698c5de1ab35b8efab536ba7a9bfa1
Storm Burpee <stormburpee@gmail.com>
2017-08-08 19:56:56 +0930

    Further search refinements

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { LoginService } from '../login.service';
 import {Router, ActivatedRoute, Params} from '@angular/router';
+import { HeaderComponent } from '../dashboard/header/header.component';
 
 @Component({
   selector: 'app-user',
@@ -11,20 +12,74 @@ import {Router, ActivatedRoute, Params} from '@angular/router';
 export class UserComponent implements OnInit {
 
   user;
+  processedEvents;
+  myEvents;
+  repos;
   username;
 
   constructor(private githubService: LoginService, private route: ActivatedRoute ) { }
 
   ngOnInit() {
-    this.username = this.route.snapshot.queryParams['username'];
+    this.username = this.route.snapshot.paramMap.get('username');
+    //this.username = "StormBurpee";
     this.getUser();
   }
 
   getUser() {
     this.githubService.getUser(this.username).subscribe(
       res => this.user = res,
-      error => null
+      error => null,
+      () => this.postUser()
     );
+  }
+
+  postUser() {
+    this.getMyEvents();
+    this.getRepos();
+  }
+
+  getRepos() {
+    this.githubService.getUserRepos(this.username).subscribe(
+      repos => this.repos = repos
+    )
+  }
+
+  getMyEvents() {
+    this.githubService.getUserEvents(this.username).subscribe(
+      resp => this.myEvents = resp,
+      error => null,
+      () => this.processEvents()
+    )
+  }
+
+  processEvents() {
+    let processed = {
+      CreateEvent: {
+        name: "CreateEvent",
+        count: 0
+      },
+      PushEvent: {
+        name: "PushEvent",
+        count: 0
+      },
+      PullRequestEvent: {
+        name: "PullRequestEvent",
+        count: 0
+      }
+    };
+    for(var i = 0; i < this.myEvents.length; i++) {
+      let event = this.myEvents[i];
+      if(processed[event.type]) {
+        processed[event.type].count += 1;
+      } else {
+        processed[event.type] = {
+          name: event.type,
+          count: 1
+        };
+      }
+    }
+    this.processedEvents = processed;
+    console.log("events", processed);
   }
 
 }
